25db1fadce8e0718cb07830e2d6eeb51afa70cb0
Mohd Talib <96992202+Mohd1999-collb@users.noreply.github.com>
2023-08-21 01:15:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+const line = document.getElementById("line");
+
+line.addEventListener("mouseover", function(){
+	console.log("first")
+	line.classList.toggle("rotateLine");
+});
