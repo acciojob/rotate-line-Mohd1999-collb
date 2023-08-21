a4ac4c72d252f46cc5f7e456d0d94d546046ca32
Mohd Talib <96992202+Mohd1999-collb@users.noreply.github.com>
2023-08-22 01:11:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 //your JS code here. If required.
-const line = document.getElementById("line");
+// const line = document.getElementById("line");
 
-line.addEventListener("mouseover", function(){
-	console.log("first")
-	line.classList.toggle("rotateLine");
-});
+// line.addEventListener("mouseover", function(){
+// 	console.log("first")
+// 	line.classList.toggle("rotateLine");
+// });
+
+const line = document.getElementById("line");
+let rotationAngle = 0;
+
+function rotate() {
+    rotationAngle += 2; //+1 to rotate Clockwise and -1 for antiClockwise
+    line.style.transform = `rotate(${rotationAngle}deg)`; //used string template to generate rotating angle
+    requestAnimationFrame(rotate); //this is recursive call
+    //to create infinite animation/rotation
+}
+
+rotate();
